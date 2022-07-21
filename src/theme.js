908a4552c427206839643f924a41cf0261236913
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-07-21 16:14:49 +0200

    theme added

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+  breakpoint: {
+    xxl: "1400px",
+    xl: "1200px",
+    lg: "992px",
+    md: "768px",
+    sm: "576px",
+    xs: "575px",
+    xxs: "375px"
+  },
+};
