c2a45d935ba209e7bfe1c510c067fac6ad717838
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-07-27 20:36:22 +0200

    build WorkshopBrowser

---
+++ src/components/core/App/index.js
@@ -0,0 +1,15 @@
+import Footer from "../../common/Footer";
+import Header from "../../common/Header";
+import { StyledPage } from "./styled";
+
+function WorkshopBrowser() {
+
+  return (
+    <StyledPage>
+      <Header />
+      <Footer />
+    </StyledPage>
+  );
+}
+
+export default WorkshopBrowser;
