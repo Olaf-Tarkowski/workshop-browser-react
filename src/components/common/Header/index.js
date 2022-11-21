b573b1e9d5e7487d086b27b93c9bd78f4af76431
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-11-21 17:25:10 +0100

    navigation added

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,10 +1,29 @@
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { StyledList, StyledNavLink } from "./styled";
 
 const Header = () => {
 
   return (
-    <div>
-      Header
-    </div>
+    <HashRouter>
+      <nav>
+        <StyledList>
+          <li><StyledNavLink to="/zadania">O mnie</StyledNavLink></li>
+          <li><StyledNavLink to="/autor">Cennik</StyledNavLink></li>
+        </StyledList>
+      </nav>
+      <Switch>
+        <Route path="/zadania/:id">
+          <div>Hejka</div>
+        </Route>
+        <Route path="/zadania">
+        <div>Hej</div>
+        </Route>
+        <Route path="/autor">
+        <div>Autor</div>
+        </Route>
+        <Redirect to="/zadania" />
+      </Switch>
+    </HashRouter>
   );
 }
 
