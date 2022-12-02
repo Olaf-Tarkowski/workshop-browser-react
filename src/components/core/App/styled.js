a84e98fe4a9bfbf2c908f17c529fefd0f486497a
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-12-02 13:35:38 +0100

    initial commit

--- src/components/core/App/styled.js
+++ src/components/core/App/styled.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const StyledPage = styled.div`
-  margin: 115px auto;
-  padding: 16px;
-  max-width: 1244px;
+  margin: auto;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.md}) {
     margin: 0px auto;
