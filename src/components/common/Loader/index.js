120e4372f161b6c74fa44515bb1a3eb1421c0a6b
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-12-12 22:17:47 +0100

    initial commit

--- src/components/common/Loader/index.js
+++ src/components/common/Loader/index.js
@@ -1,11 +1,11 @@
-import {LoaderContainer, Logo} from "./styled";
-import DarkLogo from "../../resources/images/NewSvg.svg"
-
+import { LoaderContainer, Logo, Text } from "./styled";
+import DarkLogo from "../../resources/images/FooxLogo.png";
 
 const Loader = () => {
   return (
     <LoaderContainer>
       <Logo src={DarkLogo} alt={""} />
+      <Text>Is Coming Soon</Text>
     </LoaderContainer>
   );
 };
