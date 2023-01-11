f1d7612050408329c3e4affbc1af3ed9d27ca638
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-11 19:43:52 +0100

    transition added

--- src/components/common/Navbar/styled.js
+++ src/components/common/Navbar/styled.js
@@ -2,24 +2,27 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledList = styled.ul`
-    margin: 0px;
-    padding: 0px;
-    width: 100%;
-    height: 50px;
-    background-color: rgba(43, 43, 43, 0.836);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    list-style: none;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 50px;
+  color: ${({ theme }) => theme.color.white};
+  background: ${({ theme }) => theme.color.gray};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
 `;
 export const StyledNavLink = styled(NavLink)`
-    padding: 15px;
-    text-decoration: none;
-    color: white;
-    &.active{
-        font-weight: 700
-    }
-    &:hover{
-        text-decoration: underline;
-    }
+  transition: 0.8s;
+  padding: 15px;
+  text-decoration: none;
+  color: white;
+  &.active {
+    font-weight: 700;
+    color: ${({ theme }) => theme.color.pampas};
+  }
+  &:hover {
+    color: ${({ theme }) => theme.color.black};
+  }
 `;
