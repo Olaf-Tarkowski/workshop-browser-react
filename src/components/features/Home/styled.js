916d46b0a99db1932e4e48c0ebf70b23c1cf261d
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-24 18:00:56 +0100

    styling added

---
+++ src/components/features/Home/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Img = styled.img``;
+
+export const SmaleImages = styled.img`
+  width: 120px;
+`;
+
+export const SmallBox = styled.div`
+  padding: 25px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Text = styled.div`
+  padding: 0 25px;
+  max-width: 500px;
+`;
