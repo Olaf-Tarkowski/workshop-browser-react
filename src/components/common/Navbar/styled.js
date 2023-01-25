afaf94948ac13e7c0510426e70510feae98daeb9
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-25 22:34:44 +0100

    initial commit

--- src/components/common/Navbar/styled.js
+++ src/components/common/Navbar/styled.js
@@ -1,28 +1,45 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+export const Background = styled.div`
+  background: ${({ theme }) => theme.color.gray};
+`;
+
+export const Nav = styled.nav`
+  margin: auto;
+  padding: 20px 0;
+  display: flex;
+  align-items: center;
+`;
+
 export const StyledList = styled.ul`
   margin: 0px;
   padding: 0px;
   width: 100%;
   height: 50px;
-  color: ${({ theme }) => theme.color.white};
-  background: ${({ theme }) => theme.color.gray};
+  color: ${({ theme }) => theme.color.black};
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
   list-style: none;
 `;
+
 export const StyledNavLink = styled(NavLink)`
   transition: 0.8s;
-  padding: 15px;
+  padding: 0 35px;
+  font-size: 20px;
   text-decoration: none;
-  color: white;
+  color: ${({ theme }) => theme.color.black};
   &.active {
     font-weight: 700;
-    color: ${({ theme }) => theme.color.pampas};
+    color: ${({ theme }) => theme.color.black};
   }
   &:hover {
     color: ${({ theme }) => theme.color.black};
   }
+`;
+
+export const Logo = styled.img`
+  padding: 0 35px;
+  width: 350px;
 `;
