fba1c25316a7355526acf8df1f59e9efd5d6eea3
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-25 22:35:12 +0100

    minor adjustments

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -2,33 +2,45 @@ import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import About from "../../features/About";
 import Contact from "../../features/Contact";
 import Home from "../../features/Home";
-import { StyledList, StyledNavLink } from "./styled";
+import { Background, Logo, Nav, StyledList, StyledNavLink } from "./styled";
+import DarkLogo from "../../resources/images/FooxLogo.png";
+import Container from "../Container";
 
 const Navbar = () => {
-
   return (
     <HashRouter>
-      <nav>
-        <StyledList>
-          <li><StyledNavLink to="/home">Home</StyledNavLink></li>
-          <li><StyledNavLink to="/about">About</StyledNavLink></li>
-          <li><StyledNavLink to="/contact">Contact</StyledNavLink></li>
-        </StyledList>
-      </nav>
+      <Background>
+        <Container>
+          <Nav>
+            <Logo src={DarkLogo} />
+            <StyledList>
+              <li>
+                <StyledNavLink to="/home">Home</StyledNavLink>
+              </li>
+              <li>
+                <StyledNavLink to="/about">About</StyledNavLink>
+              </li>
+              <li>
+                <StyledNavLink to="/contact">Contact</StyledNavLink>
+              </li>
+            </StyledList>
+          </Nav>
+        </Container>
+      </Background>
       <Switch>
         <Route path="/home">
-        <Home/>
+          <Home />
         </Route>
         <Route path="/about">
-        <About />
+          <About />
         </Route>
         <Route path="/contact">
-        <Contact />
+          <Contact />
         </Route>
         <Redirect to="/home" />
       </Switch>
     </HashRouter>
   );
-}
+};
 
 export default Navbar;
