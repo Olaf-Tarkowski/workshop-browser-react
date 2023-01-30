36bf8b710e96669ee1f44fcf304b40cb970ebe0c
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-30 23:43:38 +0100

    styles for component added

--- src/components/features/About/styled.js
+++ src/components/features/About/styled.js
@@ -1,1 +1,44 @@
 import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.div`
+  max-width: 600px;
+  padding: 20px;
+`;
+
+export const SectionImg = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 1060px;
+`;
+
+export const StyledImg = styled.div`
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 280px;
+  height: 300px;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+`;
+
+export const StyledImgBelow = styled.div`
+  position: relative;
+  top: -62px;
+  right: -140px;
+`;
+
+export const ImgBelow = styled.div`
+  position: relative;
+  top: -137px;
+`;
+
+export const Img = styled.img`
+  width: 650px;
+  height: 100%;
+`;
