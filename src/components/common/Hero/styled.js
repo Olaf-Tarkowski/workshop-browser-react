b563777ed4d91438fdf0f43dbaf9d80cc6f5ac40
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-06 21:42:38 +0100

    hex images added

--- src/components/common/Hero/styled.js
+++ src/components/common/Hero/styled.js
@@ -1,1 +1,51 @@
 import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+  position: absolute;
+  display: flex;
+  z-index: 99;
+`;
+
+export const Logo = styled.img`
+  max-width: 1600px;
+`;
+
+export const Section = styled.section`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items: center;
+`;
+
+export const ImgAbove = styled.div`
+  display: flex;
+`;
+export const FirstLine = styled.div`
+  display: flex;
+  position: relative;
+  height: 240px;
+`;
+export const SecondLine = styled.div`
+  display: flex;
+  position: relative;
+  height: 240px;
+`;
+export const ThirdLine = styled.div`
+  display: flex;
+`;
+
+export const StyledImg = styled.div`
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 280px;
+  height: 300px;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+`;
+
+export const Img = styled.img`
+  width: 650px;
+  height: 100%;
+`;
