5a74dd671ab638659b4e80332c974dcc6585f23e
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-13 23:26:30 +0100

    initial commit

--- src/components/common/Footer/styled.js
+++ src/components/common/Footer/styled.js
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
 export const SectionFooter = styled.footer`
-margin-top:auto;
-background: ${({ theme }) => theme.color.gray};
+  margin-top: auto;
+`;
+export const Display = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
+export const Info = styled.div`
+  padding: 10px 0;
+  background: ${({ theme }) => theme.color.grayTransparent};
+`;
+
+export const Copyrights = styled.div`
+  padding: 10px 0;
+  background: ${({ theme }) => theme.color.gray};
+`;
+
+export const Logo = styled.img`
+  height: 40px;
+`;
+
+export const Text = styled.div`
+  /* color: white; */
+  padding-left: 20px;
+`;
