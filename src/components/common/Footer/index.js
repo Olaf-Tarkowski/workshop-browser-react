894974b24c1672b5b26309631e9a5aff39650e55
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-13 23:26:17 +0100

    initial commit

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,10 +1,24 @@
 import Container from "../Container";
-import { SectionFooter } from "./styled";
+import { Copyrights, Display, Info, Logo, SectionFooter, Text } from "./styled";
+import WhiteLogo from "../../resources/images/FooxLogo.png";
 
 const Footer = () => {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
   return (
     <SectionFooter>
-      <Container>Footer</Container>
+      <Info>
+        <Container>nara</Container>
+      </Info>
+      <Copyrights>
+        <Container>
+          <Display>
+            <Logo src={WhiteLogo} />
+            <Text>Copyright {currentYear} FooxGarage. All Rights Reserved</Text>
+          </Display>
+        </Container>
+      </Copyrights>
     </SectionFooter>
   );
 };
