adc65265d2f7ecd3a4ba30ff43cd0d1148172ac0
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-15 22:04:51 +0100

    Fade added

--- src/components/features/About/index.js
+++ src/components/features/About/index.js
@@ -14,62 +14,67 @@ import Nissan350z from "../../resources/foto/350z.jpg";
 import Impreza from "../../resources/foto/impreza.jpg";
 import S2000 from "../../resources/foto/s2000.jpg";
 import Nissan370z from "../../resources/foto/370z.jpg";
+import { Fade } from "react-reveal";
+
 const About = () => {
   return (
-    <Main>
-      <Container>
-        <Section>
-          <Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-          </Text>
-          <SectionImg>
-            <ImgAbove>
-              <StyledImg>
-                <Img src={Nissan350z} />
-              </StyledImg>
-              <StyledImgBelow>
+    <Fade duration={1000} delay={100}>
+      <Main>
+        <Container>
+          <Section>
+            <Text>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not Lorem Ipsum is simply dummy text of the printing
+              and typesetting industry. Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen
+              book. It has survived not Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not Lorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not Lorem Ipsum is
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book. It has survived not
+            </Text>
+            <SectionImg>
+              <ImgAbove>
                 <StyledImg>
-                  <Img src={Impreza} />
+                  <Img src={Nissan350z} />
                 </StyledImg>
-              </StyledImgBelow>
-            </ImgAbove>
-            <ImgBelow>
-              <StyledImg>
-                <Img src={S2000} />
-              </StyledImg>
-              <StyledImgBelow>
+                <StyledImgBelow>
+                  <StyledImg>
+                    <Img src={Impreza} />
+                  </StyledImg>
+                </StyledImgBelow>
+              </ImgAbove>
+              <ImgBelow>
                 <StyledImg>
-                  <Img src={Nissan370z} />
+                  <Img src={S2000} />
                 </StyledImg>
-              </StyledImgBelow>
-            </ImgBelow>
-          </SectionImg>
-        </Section>
-      </Container>
-    </Main>
+                <StyledImgBelow>
+                  <StyledImg>
+                    <Img src={Nissan370z} />
+                  </StyledImg>
+                </StyledImgBelow>
+              </ImgBelow>
+            </SectionImg>
+          </Section>
+        </Container>
+      </Main>
+    </Fade>
   );
 };
 
