a82e6f64f1d687aac64e2ca7331e4a5156c4a8ae
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-03-20 21:12:56 +0100

    hamburger menu added

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -2,28 +2,49 @@ import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import About from "../../features/About";
 import Contact from "../../features/Contact";
 import Home from "../../features/Home";
-import { Section, Logo, Nav, StyledList, StyledNavLink } from "./styled";
+import {
+  Section,
+  Logo,
+  Nav,
+  StyledNavLink,
+  HamburgerMenuContainer,
+  NavSection,
+} from "./styled";
 import DarkLogo from "../../resources/images/FooxLogo.png";
 import Container from "../Container";
+import HamburgerMenu from "react-hamburger-menu";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <HashRouter>
       <Section>
         <Container>
           <Nav>
             <Logo src={DarkLogo} />
-            <StyledList>
-              <li>
-                <StyledNavLink to="/home">Home</StyledNavLink>
-              </li>
-              <li>
-                <StyledNavLink to="/about">About</StyledNavLink>
-              </li>
-              <li>
-                <StyledNavLink to="/contact">Contact</StyledNavLink>
-              </li>
-            </StyledList>
+            <NavSection isOpen={isOpen}>
+              <StyledNavLink to="/home">Home</StyledNavLink>
+              <StyledNavLink to="/about">About</StyledNavLink>
+              <StyledNavLink to="/contact">Contact</StyledNavLink>
+            </NavSection>
+            <HamburgerMenuContainer>
+              <HamburgerMenu
+                isOpen={isOpen}
+                menuClicked={handleClick}
+                width={24}
+                height={20}
+                strokeWidth={2}
+                rotate={0}
+                color={isOpen ? "#000000" : "#000000"}
+                borderRadius={0}
+                animationDuration={0.5}
+              />
+            </HamburgerMenuContainer>
           </Nav>
         </Container>
       </Section>
